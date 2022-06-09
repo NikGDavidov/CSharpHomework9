d013156a2c479353644af3f7b8c7a46e57e24f30
Nikolay_Davydov <skmoscow74@yandex.ru>
2022-06-09 13:03:33 +0300

    homework9

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+void PrintEvenNumbers (int from, int to)
+{
+    if (from > to) return;
+    if (from%2 == 0) Console.Write (from + ", ");
+    PrintEvenNumbers (from+1, to);
+}
+int from = 4;
+int to = 8;
+PrintEvenNumbers (from,to);
---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int sum = 0;
+void SumNumbers (int from, int to)
+{
+    if (from > to) return;
+    sum += from;
+    SumNumbers(from+1, to); 
+}
+int m = 4;
+int n = 8;
+SumNumbers(m,n);
+Console.WriteLine ($"M = {m}; N = {n} -> {sum}");
